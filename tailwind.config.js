1059c9660f6ec43e19adbdb4e6e22df0d3693283
Kamil Bieniek <kamilb231@op.pl>
2023-05-08 12:33:07 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    colors: {
+      'white': 'hsl(0, 0%, 100%)',
+      'ligh-gray': 'hsl(212, 45%, 89%)',
+      'Grayish-blue': 'hsl(220, 15%, 55%)',
+      'Dark-blue': 'hsl(218, 44%, 22%)',
+    },
+    fontFamily: {
+      main: ['Outfit', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
